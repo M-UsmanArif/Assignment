14ae9a2177608786a165c4c8e5783c643d92d83c
Faizan <faizan.nasir147@gmail.com>
2022-02-13 11:03:11 +0500

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Admin on 13/02/2022.
+//  Copyright © 2022 Brain Plow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
